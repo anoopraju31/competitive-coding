2bfbe8549fe30a9d6d88a1c9b3f22ac263935bbb
anoopraju31 <anoopraju31@gmail.com>
2024-03-21 19:25:33 +0530

    CSES - Dice Combinations

--- cses/DiceCombinations/index.js
+++ cses/DiceCombinations/index.js
@@ -1,20 +1,22 @@
+var readline = require('readline')
+
+var r = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false,
+})
+const MOD = 1000000007
 const diceCombinators = (n) => {
-	if (n == 1 || n == 0) return 1
+	const dp = new Array(n + 1).fill(0)
+	dp[0] = 1
 
-	const dp = new Array(n + 1).fill(-1)
+	for (let i = 1; i <= n; i++)
+		for (let j = 1; j <= 6; j++) if (j <= i) dp[i] = (dp[i] + dp[i - j]) % MOD
 
-	const getCount = (i) => {
-		if (i < 0) return 0
-		if (i == 1 || i == 0) return 1
-		if (dp[i] !== -1) return dp[i]
-
-		dp[i] = 0
-		for (let j = 1; j <= 6; j++) dp[i] += getCount(i - j)
-
-		return dp[i]
-	}
-
-	return getCount(n)
+	return dp[n]
 }
 
-module.exports = diceCombinators
+r.on('line', function (line) {
+	var vals = line.split(' ')
+	console.log(diceCombinators(parseInt(vals[0])))
+})
