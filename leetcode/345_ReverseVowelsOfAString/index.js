9f1f7fa26263bcda593c5fdfa91dc3d62fc1e2de
anoopraju31 <anoopraju31@gmail.com>
2024-04-06 12:10:26 +0530

    Leetcode: 345. Reverse Vowels of a String - approach 2

--- leetcode/345_ReverseVowelsOfAString/index.js
+++ leetcode/345_ReverseVowelsOfAString/index.js
@@ -19,3 +19,31 @@ var reverseVowels = function (s) {
 
 const s = 'hello'
 console.log(reverseVowels(s))
+
+// Approach 2
+const isVowel = (c) =>
+	c === 'a' || c === 'e' || c === 'i' || c === 'o' || c === 'u'
+
+var reverseVowels = function (s) {
+	str = s.split('')
+	let l = 0
+	let r = s.length - 1
+
+	while (l < r) {
+		let sl = s[l].toLowerCase()
+		let sr = s[r].toLowerCase()
+
+		if (!isVowel(sl)) l++
+		if (!isVowel(sr)) r--
+
+		if (isVowel(sl) && isVowel(sr)) {
+			let temp = str[l]
+			str[l] = str[r]
+			str[r] = temp
+			r--
+			l++
+		}
+	}
+
+	return str.join('')
+}
