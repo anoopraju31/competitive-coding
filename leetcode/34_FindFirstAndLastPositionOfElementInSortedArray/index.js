0aa22683b5c565f74caff4e7979dfc206fe6e18a
anoopraju31 <anoopraju31@gmail.com>
2024-04-06 16:46:51 +0530

    Leetcode: 34. Find First and Last Position of Element in Sorted Array - Approach 2

--- leetcode/34_FindFirstAndLastPositionOfElementInSortedArray/index.js
+++ leetcode/34_FindFirstAndLastPositionOfElementInSortedArray/index.js
@@ -30,3 +30,27 @@ var searchRange = function (nums, target) {
 
 	return [leftBound, rightBound]
 }
+
+// Binary Search
+// Time: O(logn)
+// Space: O(1)
+// if target found, mid may be somewhere between the starting and ending index so check both ways until no equal the target and return the index.
+var searchRange = function (nums, target) {
+	let l = 0
+	let r = nums.length - 1
+
+	while (l <= r) {
+		let mid = Math.floor((l + r) / 2)
+
+		if (nums[mid] === target) {
+			let t = mid
+			while (nums[t] === target) t--
+			while (nums[mid] === target) mid++
+
+			return [t + 1, mid - 1]
+		} else if (nums[mid] < target) l = mid + 1
+		else r = mid - 1
+	}
+
+	return [-1, -1]
+}
