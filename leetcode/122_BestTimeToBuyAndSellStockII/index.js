12488d1fb2499ed3783a9626a9a74a9f2c6f2377
anoopraju31 <anoopraju31@gmail.com>
2024-04-06 11:19:52 +0530

    Leetcode: 122. Best Time to Buy and Sell Stock II - refactor

--- leetcode/122_BestTimeToBuyAndSellStockII/index.js
+++ leetcode/122_BestTimeToBuyAndSellStockII/index.js
@@ -1,4 +1,4 @@
-const maxProfit = (nums) => {
+var maxProfit = (nums) => {
 	let left = 0
 	let right = 0
 	let n = nums.length - 1
@@ -9,6 +9,18 @@ const maxProfit = (nums) => {
 		else profit += nums[right + 1] - nums[right]
 
 		right++
+	}
+
+	return profit
+}
+
+var maxProfit = function (prices) {
+	let r = 0
+	let profit = 0
+
+	while (r < prices.length - 1) {
+		if (prices[r + 1] - prices[r] > 0) profit += prices[r + 1] - prices[r]
+		r++
 	}
 
 	return profit
